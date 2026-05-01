f62527ba509c974a0f8c541899e642c2e7c3e463
Anton Maslov <tosha.maslov.1999@mail.ru>
2026-05-01 18:08:51 +0300

    2

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,31 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfig } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import pluginVue from "eslint-plugin-vue"
+import { defineConfig } from "eslint/config"
+import prettierPlugin from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
     plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser }
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "no-console": "error",
+      curly: "warn",
+      eqeqeq: "error",
+    },
   },
   tseslint.configs.recommended,
-  pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } }
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
 ])
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function onClick() {
+  for (let i = 0; i < 10; i++) {
+    if (i === 2) {
+      return ""
+    }
+    return ""
+    console.log(i)
+    continue
+    return i
+  }
+}
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
+    read the documentation
   </p>
 </template>
 
-<style scoped></style>
+<style scoped>
+.a {
+}
+
+.b {
+}
+</style>
